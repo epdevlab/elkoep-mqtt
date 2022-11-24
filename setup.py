f3dfe71afe6d37839be102d46b6802ad89351cbc
ZeD4805 <jp.baltazar.mendes@gmail.com>
2022-11-24 13:05:22 +0100

    updated reqs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="inels-mqtt-dev",
-    version="0.0.10",
+    version="0.0.11",
     url="https://github.com/zed4805/inels-mqtt-dev",
     license="MIT",
     author="Elko EP s.r.o.",
