2fdbd33fab72db87e996a428efd27d8882093b7b
ZeD4805 <jp.baltazar.mendes@gmail.com>
2023-02-22 15:06:06 +0000

    changed lib version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="inels-mqtt-new",
-    version="0.2.1b16",
+    version="0.2.1",
     url="https://github.com/zed4805/inels-mqtt-new",
     license="MIT",
     author="Elko EP s.r.o.",
