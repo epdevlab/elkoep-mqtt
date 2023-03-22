59c72d00d03870ad8bc4f589b949b406fda9489d
ZeD4805 <zed4805@gmail.com>
2023-03-22 15:59:36 +0000

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="inels-mqtt-new",
-    version="0.2.12a4",
+    version="0.2.12a5",
     url="https://github.com/zed4805/inels-mqtt-new",
     license="MIT",
     author="Elko EP s.r.o.",
