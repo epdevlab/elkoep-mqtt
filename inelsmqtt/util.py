625b268d68c4f3df3bac1ecaa98be03a7339850f
ZeD4805 <zed4805@gmail.com>
2023-07-01 10:00:15 +0100

    fixed names

--- inelsmqtt/util.py
+++ inelsmqtt/util.py
@@ -1601,10 +1601,10 @@ class DeviceValue(object):
                     temp_current = int(self.__trim_inels_status_values(
                         DEVICE_TYPE_166_DATA, CURRENT_TEMP, ""
                     ), 16) / 100
-                    if temp_required_heat == 0x7FFFFFFB:
-                        temp_required_heat = None
+                    if temp_current == 0x7FFFFFFB:
+                        temp_current = None
                     else:
-                        temp_required_heat /= 100
+                        temp_current /= 100
                     temp_critical_max = int(self.__trim_inels_status_values( #check if 0x7F FF FF FB -> make it 50
                         DEVICE_TYPE_166_DATA, CRITICAL_MAX_TEMP, ""
                     ), 16) / 100
